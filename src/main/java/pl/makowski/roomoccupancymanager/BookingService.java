8b6edd3750f25dc6c92b902773ac9b6ae43fb2bc
makowskimateusz <mateusz.makos@gmail.com>
2021-04-15 12:54:42 +0200

    All test scenarios covered

--- src/main/java/pl/makowski/roomoccupancymanager/BookingService.java
+++ src/main/java/pl/makowski/roomoccupancymanager/BookingService.java
@@ -1,10 +1,16 @@
 package pl.makowski.roomoccupancymanager;
 
 import java.util.ArrayDeque;
-import java.util.function.DoublePredicate;
 import java.util.stream.DoubleStream;
 
 import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
+import static pl.makowski.roomoccupancymanager.BookingType.ECONOMY;
+import static pl.makowski.roomoccupancymanager.BookingType.PREMIUM;
+
+enum BookingType {
+    PREMIUM, ECONOMY
+}
 
 public class BookingService {
 
@@ -12,38 +18,75 @@ public class BookingService {
     private static final double PRICE_THRESHOLD = 100.00;
 
     public BookingCalculation calculateRooms(long premiumRoomsCount, long economyRoomsCount) {
-        final var offersBelow100Eur = getCollect(below100Eur());
-        final var offersAbove100Eur = getCollect(above100Eur());
+        final var bookings = DoubleStream.of(clientsOffers)
+                .sorted()
+                .mapToObj(clientOffer -> {
+                    if (clientOffer < PRICE_THRESHOLD) {
+                        return new Booking(clientOffer, ECONOMY);
+                    } else {
+                        return new Booking(clientOffer, PREMIUM);
+                    }
+                }).collect(toList());
 
-        final var economyIncome = countIncome(economyRoomsCount, offersBelow100Eur);
-        final var premiumIncome = countIncome(premiumRoomsCount, offersAbove100Eur);
+        final var premiumBookings = bookings.stream().filter(booking -> booking.getType().equals(PREMIUM)).collect(toCollection(ArrayDeque::new));
+        final var economyBookings = bookings.stream().filter(booking -> booking.getType().equals(ECONOMY)).collect(toCollection(ArrayDeque::new));
+
+        ArrayDeque<Booking> premiumAndUpgradedBookings = new ArrayDeque<>();
+        premiumAndUpgradedBookings.addAll(premiumBookings);
+        if (premiumBookings.size() < premiumRoomsCount && !(economyRoomsCount > economyBookings.size())) {
+            for (int i = 0; i < premiumRoomsCount - premiumBookings.size(); i++) {
+                final var upgradedBooking = economyBookings.pollLast();
+                if (upgradedBooking != null) {
+                    premiumAndUpgradedBookings.addFirst(upgradedBooking);
+                }
+            }
+        }
+
+        double premiumIncome = 0;
+
+        for (int i = 0; i < premiumRoomsCount; i++) {
+            final var booking = premiumAndUpgradedBookings.pollLast();
+            if (booking != null) {
+                premiumIncome = premiumIncome + booking.getPrice();
+            }
+        }
+
+        double economyIncome = 0;
+
+        for (int i = 0; i < economyRoomsCount; i++) {
+            final var booking = economyBookings.pollLast();
+            if (booking != null) {
+                economyIncome = economyIncome + booking.getPrice();
+            }
+        }
 
         return new BookingCalculation(economyIncome, premiumIncome);
     }
 
-    private ArrayDeque<Double> getCollect(DoublePredicate priceLevelPredicate) {
-        return DoubleStream.of(clientsOffers)
-                .sorted()
-                .filter(priceLevelPredicate)
-                .boxed()
-                .collect(toCollection(ArrayDeque::new));
-    }
-
-    private DoublePredicate above100Eur() {
-        return offer -> offer > PRICE_THRESHOLD;
-    }
-
-    private DoublePredicate below100Eur() {
-        return offer -> offer < PRICE_THRESHOLD;
-    }
-
-    private double countIncome(long roomsCount, ArrayDeque<Double> offers) {
-        double sum = 0;
-        for (int i = 0; i < roomsCount; i++) {
-            sum = sum + offers.pollLast();
-        }
-        return sum;
-    }
-
 }
+
+class Booking implements Comparable<Booking> {
+
+    private final Double price;
+    private final BookingType type;
+
+    public Booking(double price, BookingType type) {
+        this.price = price;
+        this.type = type;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public BookingType getType() {
+        return type;
+    }
+
+    @Override
+    public int compareTo(Booking booking) {
+        return booking.price.compareTo(this.price);
+    }
+}
+
 
--- src/test/java/pl/makowski/roomoccupancymanager/BookingServiceTest.java
+++ src/test/java/pl/makowski/roomoccupancymanager/BookingServiceTest.java
@@ -17,9 +17,43 @@ public class BookingServiceTest {
         //when
         final var result = bookingService.calculateRooms(premiumRoomsCount, economyRoomsCount);
         //then
-        assertThat(result.premium()).isEqualTo(738);
         assertThat(result.economy()).isEqualTo(167.99);
+        assertThat(result.premium()).isEqualTo(738);
     }
 
+    @Test
+    public void shouldReturnProposedIncomeForEconomyAndPremiumRooms2() {
+        //given
+        final long premiumRoomsCount = 7;
+        final long economyRoomsCount = 5;
+        //when
+        final var result = bookingService.calculateRooms(premiumRoomsCount, economyRoomsCount);
+        //then
+        assertThat(result.premium()).isEqualTo(1054);
+        assertThat(result.economy()).isEqualTo(189.99);
+    }
 
+    @Test
+    public void shouldReturnProposedIncomeForEconomyAndPremiumRooms3() {
+        //given
+        final long premiumRoomsCount = 2;
+        final long economyRoomsCount = 7;
+        //when
+        final var result = bookingService.calculateRooms(premiumRoomsCount, economyRoomsCount);
+        //then
+        assertThat(result.premium()).isEqualTo(583);
+        assertThat(result.economy()).isEqualTo(189.99);
+    }
+
+    @Test
+    public void shouldReturnProposedIncomeForEconomyAndPremiumRooms4() {
+        //given
+        final long premiumRoomsCount = 7;
+        final long economyRoomsCount = 1;
+        //when
+        final var result = bookingService.calculateRooms(premiumRoomsCount, economyRoomsCount);
+        //then
+        assertThat(result.premium()).isEqualTo(1153.99);
+        assertThat(result.economy()).isEqualTo(45);
+    }
 }
