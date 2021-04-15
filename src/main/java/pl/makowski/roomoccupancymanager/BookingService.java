9299a8ee6f2fc7ff1859ec135bb53985bf7bded7
makowskimateusz <mateusz.makos@gmail.com>
2021-04-15 13:01:41 +0200

    Refactor: BookingService.java cleanup, methods extracted with understandable names

--- src/main/java/pl/makowski/roomoccupancymanager/BookingService.java
+++ src/main/java/pl/makowski/roomoccupancymanager/BookingService.java
@@ -1,6 +1,7 @@
 package pl.makowski.roomoccupancymanager;
 
 import java.util.ArrayDeque;
+import java.util.List;
 import java.util.stream.DoubleStream;
 
 import static java.util.stream.Collectors.toCollection;
@@ -18,19 +19,32 @@ public class BookingService {
     private static final double PRICE_THRESHOLD = 100.00;
 
     public BookingCalculation calculateRooms(long premiumRoomsCount, long economyRoomsCount) {
-        final var bookings = DoubleStream.of(clientsOffers)
-                .sorted()
-                .mapToObj(clientOffer -> {
-                    if (clientOffer < PRICE_THRESHOLD) {
-                        return new Booking(clientOffer, ECONOMY);
-                    } else {
-                        return new Booking(clientOffer, PREMIUM);
-                    }
-                }).collect(toList());
+        final List<Booking> bookings = categorizeOffersByBookingType();
 
-        final var premiumBookings = bookings.stream().filter(booking -> booking.getType().equals(PREMIUM)).collect(toCollection(ArrayDeque::new));
-        final var economyBookings = bookings.stream().filter(booking -> booking.getType().equals(ECONOMY)).collect(toCollection(ArrayDeque::new));
+        final var premiumBookings = getBookingsByType(bookings, PREMIUM);
+        final var economyBookings = getBookingsByType(bookings, ECONOMY);
 
+        ArrayDeque<Booking> premiumAndUpgradedBookings = tryUpgradeEconomyBookingsToSatisfyRequirments(premiumRoomsCount, economyRoomsCount, premiumBookings, economyBookings);
+
+        double premiumIncome = calculateIncome(premiumRoomsCount, premiumAndUpgradedBookings);
+        double economyIncome = calculateIncome(economyRoomsCount, economyBookings);
+
+        return new BookingCalculation(economyIncome, premiumIncome);
+    }
+
+    private double calculateIncome(long roomCount, ArrayDeque<Booking> bookings) {
+        double income = 0;
+
+        for (int i = 0; i < roomCount; i++) {
+            final var booking = bookings.pollLast();
+            if (booking != null) {
+                income = income + booking.getPrice();
+            }
+        }
+        return income;
+    }
+
+    private ArrayDeque<Booking> tryUpgradeEconomyBookingsToSatisfyRequirments(long premiumRoomsCount, long economyRoomsCount, ArrayDeque<Booking> premiumBookings, ArrayDeque<Booking> economyBookings) {
         ArrayDeque<Booking> premiumAndUpgradedBookings = new ArrayDeque<>();
         premiumAndUpgradedBookings.addAll(premiumBookings);
         if (premiumBookings.size() < premiumRoomsCount && !(economyRoomsCount > economyBookings.size())) {
@@ -41,26 +55,26 @@ public class BookingService {
                 }
             }
         }
+        return premiumAndUpgradedBookings;
+    }
 
-        double premiumIncome = 0;
+    private ArrayDeque<Booking> getBookingsByType(List<Booking> bookings, BookingType bookingType) {
+        return bookings
+                .stream()
+                .filter(booking -> booking.getType().equals(bookingType))
+                .collect(toCollection(ArrayDeque::new));
+    }
 
-        for (int i = 0; i < premiumRoomsCount; i++) {
-            final var booking = premiumAndUpgradedBookings.pollLast();
-            if (booking != null) {
-                premiumIncome = premiumIncome + booking.getPrice();
-            }
-        }
-
-        double economyIncome = 0;
-
-        for (int i = 0; i < economyRoomsCount; i++) {
-            final var booking = economyBookings.pollLast();
-            if (booking != null) {
-                economyIncome = economyIncome + booking.getPrice();
-            }
-        }
-
-        return new BookingCalculation(economyIncome, premiumIncome);
+    private List<Booking> categorizeOffersByBookingType() {
+        return DoubleStream.of(clientsOffers)
+                .sorted()
+                .mapToObj(clientOffer -> {
+                    if (clientOffer < PRICE_THRESHOLD) {
+                        return new Booking(clientOffer, ECONOMY);
+                    } else {
+                        return new Booking(clientOffer, PREMIUM);
+                    }
+                }).collect(toList());
     }
 
 }
