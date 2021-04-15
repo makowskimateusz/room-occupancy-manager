c04a828d2231c976eb21fb77beb773e31b4d671a
makowskimateusz <mateusz.makos@gmail.com>
2021-04-15 12:56:11 +0200

    Refactor: moved Booking.java to separeted file

---
+++ src/main/java/pl/makowski/roomoccupancymanager/Booking.java
@@ -0,0 +1,25 @@
+package pl.makowski.roomoccupancymanager;
+
+class Booking implements Comparable<Booking> {
+
+    private final Double price;
+    private final BookingType type;
+
+    public Booking(double price, BookingType type) {
+        this.price = price;
+        this.type = type;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public BookingType getType() {
+        return type;
+    }
+
+    @Override
+    public int compareTo(Booking booking) {
+        return booking.price.compareTo(this.price);
+    }
+}
--- src/main/java/pl/makowski/roomoccupancymanager/BookingService.java
+++ src/main/java/pl/makowski/roomoccupancymanager/BookingService.java
@@ -65,28 +65,4 @@ public class BookingService {
 
 }
 
-class Booking implements Comparable<Booking> {
-
-    private final Double price;
-    private final BookingType type;
-
-    public Booking(double price, BookingType type) {
-        this.price = price;
-        this.type = type;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public BookingType getType() {
-        return type;
-    }
-
-    @Override
-    public int compareTo(Booking booking) {
-        return booking.price.compareTo(this.price);
-    }
-}
-
 
